873c1476602f69636715ef1d7e36e6d3299d0dc0
Thomas <tberrios9800@gmail.com>
2025-08-17 01:06:40 -0400

    Updated commend in preload index

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,7 +1,7 @@
 import { contextBridge } from 'electron';
 import * as api from './api';
 
-// Use `contextBridge` APIs to expose Electron APIs to renderer
+// Use `contextBridge` APIs to expose our custom APIs to the renderer process
 try {
   contextBridge.exposeInMainWorld('api', api);
 } catch (error) {
