e30fb2b39d09390234bf0eba8b940bb3e9724761
Thomas <tberrios9800@gmail.com>
2025-08-17 01:18:03 -0400

    Enabled sandboxing and disabled nodeIntegration to be more secure and keep concerns separated

--- src/main/index.ts
+++ src/main/index.ts
@@ -14,7 +14,9 @@ function createWindow(): void {
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+      contextIsolation: true,
+      sandbox: true,
+      nodeIntegration: false
     }
   });
 
