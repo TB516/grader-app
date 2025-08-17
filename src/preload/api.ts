825cbdda8459d97e9d6c1513c92f08ced6132d9b
Thomas <tberrios9800@gmail.com>
2025-08-17 01:05:49 -0400

    Cleaned up preload and removed entire electron api exposure

---
+++ src/preload/api.ts
@@ -0,0 +1,3 @@
+/// Bridge file that exposes the IPC emitter wrappers to the renderer through the window API
+/// Functions can be exported directly and will be auto wrapped by the contextBridge
+import { ipcRenderer } from 'electron';
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,22 +1,9 @@
 import { contextBridge } from 'electron';
-import { electronAPI } from '@electron-toolkit/preload';
+import * as api from './api';
 
-// Custom APIs for renderer
-const api = {};
-
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
-if (process.contextIsolated) {
-  try {
-    contextBridge.exposeInMainWorld('electron', electronAPI);
-    contextBridge.exposeInMainWorld('api', api);
-  } catch (error) {
-    console.error(error);
-  }
-} else {
-  // @ts-ignore (define in dts)
-  window.electron = electronAPI;
-  // @ts-ignore (define in dts)
-  window.api = api;
+// Use `contextBridge` APIs to expose Electron APIs to renderer
+try {
+  contextBridge.exposeInMainWorld('api', api);
+} catch (error) {
+  console.error(error);
 }
