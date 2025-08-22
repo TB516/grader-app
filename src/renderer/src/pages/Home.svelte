565a62d060abdbbcbf735009ae8ea95714d95c90
Thomas <tberrios9800@gmail.com>
2025-08-22 02:36:05 -0400

    Created basic home element outline

--- src/renderer/src/pages/Home.svelte
+++ src/renderer/src/pages/Home.svelte
@@ -1,1 +1,24 @@
-<h1>IGME 430 Grader App</h1>
+<script lang="ts">
+  import projects from '../../../../resources/projects.json';
+
+  let testing = false;
+  let projectUrl: string;
+  let selectedProject: string;
+
+  const testProject = (): void => {};
+</script>
+
+<main>
+  <input type="search" placeholder="Project URL" bind:value={projectUrl} />
+
+  <select bind:value={selectedProject}>
+    {#each projects as project, i (i)}
+      <option>{project}</option>
+    {/each}
+  </select>
+
+  <button onclick={testProject} disabled={testing}>Test Project</button>
+</main>
+
+<style>
+</style>
