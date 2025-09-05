0dfa9c040b274be260faaf4345ac857789dd3570
Thomas <tberrios9800@gmail.com>
2025-09-05 14:36:36 -0400

    Added listener for launch test window event

--- src/main/ipc-setup.ts
+++ src/main/ipc-setup.ts
@@ -1,5 +1,10 @@
 import { ipcMain } from 'electron';
+import createWindow from './create-window';
 
-const setupIPC = () => {};
+const setupIPC = () => {
+  ipcMain.on('launchTestWindow', (_e: Electron.IpcMainEvent, assignment: string, url: string) => {
+    createWindow('/results', { assignment, url });
+  });
+};
 
 export default setupIPC;
