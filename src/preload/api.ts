c6239d9c5dfa0e0b84e6ed5d6aba40bfd48a9d0d
Thomas <tberrios9800@gmail.com>
2025-09-05 10:58:02 -0400

    Added test launcher api

--- src/preload/api.ts
+++ src/preload/api.ts
@@ -1,3 +1,9 @@
 /// Bridge file that exposes the IPC emitter wrappers to the renderer through the window API
 /// Functions can be exported directly and will be auto wrapped by the contextBridge
 import { ipcRenderer } from 'electron';
+
+const launchTester = (assignment: string, url: string) => {
+  ipcRenderer.send('launchTestWindow', assignment, url);
+};
+
+export { launchTester };
