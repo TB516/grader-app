b557fff9d3397164ed3c9eeb156fc0c2b834b049
Thomas <tberrios9800@gmail.com>
2025-09-05 09:24:10 -0400

    Added query params to create window

--- src/main/create-window.ts
+++ src/main/create-window.ts
@@ -3,7 +3,12 @@ import { join } from 'path';
 import { is } from '@electron-toolkit/utils';
 import icon from '../../resources/icon.png?asset';
 
-function createWindow(hash: string = '/'): void {
+/**
+ * Creates a new app window
+ * @param hash String value of the hash path value
+ * @param query Record containing keys and values for the query parameters
+ */
+function createWindow(hash: string = '/', query: Record<string, string> = {}): void {
   // Create the browser window.
   const window = new BrowserWindow({
     width: 900,
@@ -31,9 +36,11 @@ function createWindow(hash: string = '/'): void {
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    window.loadURL(`${process.env['ELECTRON_RENDERER_URL']}#${hash}`);
+    window.loadURL(
+      `${process.env['ELECTRON_RENDERER_URL']}#${hash}?${new URLSearchParams(query).toString()}`
+    );
   } else {
-    window.loadFile(join(__dirname, '../renderer/index.html'), { hash: hash });
+    window.loadFile(join(__dirname, '../renderer/index.html'), { hash: hash, query: query });
   }
 }
 
