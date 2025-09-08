e6a92a95148c34f419f8dd341b6304980d530939
Thomas <tberrios9800@gmail.com>
2025-09-08 10:17:37 -0400

    Created test router function

--- src/main/tests/index.ts
+++ src/main/tests/index.ts
@@ -1,1 +1,9 @@
-export {};
+/**
+ * Takes the assignment and invokes the corresponding test suit
+ * @param _e Electron Invoke Event
+ * @param assignment Assignment to route by
+ * @param url Project URL
+ */
+const testRouter = (_e: Electron.IpcMainInvokeEvent, assignment: string, url: string) => {};
+
+export default testRouter;
