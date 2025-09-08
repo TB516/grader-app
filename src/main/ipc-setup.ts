07dab99a5f34c327fdb9ed611ea9f365eb622bd6
Thomas <tberrios9800@gmail.com>
2025-09-08 10:17:54 -0400

    Added ipc listener for testing a project

--- src/main/ipc-setup.ts
+++ src/main/ipc-setup.ts
@@ -1,10 +1,13 @@
 import { ipcMain } from 'electron';
 import createWindow from './create-window';
+import testRouter from './tests';
 
 const setupIPC = () => {
   ipcMain.on('launchTestWindow', (_e: Electron.IpcMainEvent, assignment: string, url: string) => {
     createWindow('/results', { assignment, url });
   });
+
+  ipcMain.handle('testProject', testRouter);
 };
 
 export default setupIPC;
