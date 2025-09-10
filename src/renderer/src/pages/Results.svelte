4c09ad50a1dd79f3ead63c9e6139e8c6651e31a7
Thomas <tberrios9800@gmail.com>
2025-09-09 23:00:03 -0400

    Added base for Results page

---
+++ src/renderer/src/pages/Results.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { searchParams } from 'sv-router';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    const projUrl = searchParams.get('url');
+    const projType = searchParams.get('assignment');
+  });
+</script>
