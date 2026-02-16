8d650cf89af804fd63662bd5b324a7e0179b3eed
Thomas <tberrios9800@gmail.com>
2026-02-15 23:14:57 -0500

    Redesigned the html for main page

--- src/renderer/src/pages/Home.svelte
+++ src/renderer/src/pages/Home.svelte
@@ -1,25 +1,31 @@
 <script lang="ts">
   import { projects, type ProjectTypes } from '../../../../resources/projects';
 
+  let selectedProjectType: ProjectTypes = 'Simple HTTP';
   let projectUrl: string;
-  let selectedProject: ProjectTypes;
 
-  const startTests = (): void => {
-    window.api.launchTester(selectedProject, projectUrl);
+  const handleSubmit = (): void => {
+    window.api.launchTester(selectedProjectType, projectUrl);
   };
 </script>
 
+<header>
+  <nav>
+    {#each projects as project (project)}
+      <button disabled={project === selectedProjectType} onclick={() => (selectedProjectType = project)}>
+        {project}
+      </button>
+    {/each}
+  </nav>
+
+  <h1>{selectedProjectType}</h1>
+</header>
+
 <main>
-  <form>
-    <input type="search" placeholder="Project URL" bind:value={projectUrl} />
+  <form onsubmit={handleSubmit}>
+    <input type="url" required placeholder="Project URL" bind:value={projectUrl} />
 
-    <select bind:value={selectedProject}>
-      {#each projects as project, i (i)}
-        <option>{project}</option>
-      {/each}
-    </select>
-
-    <button onclick={startTests}>Test Project</button>
+    <button type="submit">Test Project</button>
   </form>
 </main>
 
