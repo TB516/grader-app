be776b966f1fb577d9c438d991852ec389b023a6
Thomas <tberrios9800@gmail.com>
2026-02-19 00:01:58 -0500

    Update createwindow calls in index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -20,12 +20,12 @@ app.whenReady().then(() => {
   // Sets up all the IPC handlers for the main process to keep this file cleaner
   setupIPC();
 
-  createWindow();
+  createWindow({ path: '/', query: {} });
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) createWindow({ path: '/', query: {} });
   });
 });
 
