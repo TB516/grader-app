8397cd4cc4b7416d9ff71986396efab29260d4e2
Thomas <77514919+TB516@users.noreply.github.com>
2026-03-25 02:30:22 -0400

    Added eslint-plugin-import-x

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'eslint/config';
 import tseslint from '@electron-toolkit/eslint-config-ts';
 import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier';
 import eslintPluginSvelte from 'eslint-plugin-svelte';
+import { importX } from 'eslint-plugin-import-x';
 
 export default defineConfig(
   { ignores: ['**/node_modules', '**/dist', '**/out'] },
   tseslint.configs.recommended,
   eslintPluginSvelte.configs['flat/recommended'],
+  importX.flatConfigs.recommended,
+  importX.flatConfigs.typescript,
   {
     files: ['**/*.svelte'],
     languageOptions: {
@@ -16,14 +19,9 @@ export default defineConfig(
     }
   },
   {
-    files: ['**/*.{tsx,svelte}'],
     rules: {
-      'svelte/no-unused-svelte-ignore': 'off'
-    }
-  },
-  {
-    rules: {
-      '@typescript-eslint/explicit-function-return-type': 'off'
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'import-x/order': 'error'
     }
   },
   eslintConfigPrettier
