94e9e5d6683f2d3de0c8a1c40191db0d6a9e02ef
Thomas <77514919+TB516@users.noreply.github.com>
2026-03-25 04:35:47 -0400

    Updated mocks/index.ts with new imports

--- tests/main/projects/simple-http/mocks/index.ts
+++ tests/main/projects/simple-http/mocks/index.ts
@@ -1,4 +1,4 @@
 import { setupServer } from 'msw/node';
-import indexHandler from './index-page-handler';
+import { indexPageHandler } from './index-page-handler';
 
-export const server = setupServer(...[indexHandler]);
+export const server = setupServer(...[indexPageHandler]);
