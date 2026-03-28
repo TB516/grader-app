02e0cc83daab52c062a5f790bf2815f88bdec603
Thomas <77514919+TB516@users.noreply.github.com>
2026-03-28 19:16:25 -0400

    Changed mock url for simple-http

--- tests/main/projects/simple-http/mocks/index-page-handlers.ts
+++ tests/main/projects/simple-http/mocks/index-page-handlers.ts
@@ -1,21 +1,21 @@
 import { http, HttpResponse } from 'msw';
 
-export const goodIndexPageUrl = 'https://good.project.com';
+export const goodIndexPageUrl = 'https://good.simple-http.com';
 export const goodIndexPageHandler = http.get(goodIndexPageUrl, () => {
   return HttpResponse.html();
 });
 
-export const badIndexPageUrl = 'https://bad.project.com';
+export const badIndexPageUrl = 'https://bad.simple-http.com';
 export const badIndexPageHandler = http.get(badIndexPageUrl, () => {
   return HttpResponse.json();
 });
 
-export const httpErrorIndexPageUrl = 'https://httperror.project.com';
+export const httpErrorIndexPageUrl = 'https://httperror.simple-http.com';
 export const httpErrorIndexPageHandler = http.get(httpErrorIndexPageUrl, () => {
   return new HttpResponse(null, { status: 404 });
 });
 
-export const networkErrorIndexPageUrl = 'https://neterror.project.com';
+export const networkErrorIndexPageUrl = 'https://neterror.simple-http.com';
 export const networkErrorIndexPageHandler = http.get(httpErrorIndexPageUrl, () => {
   return HttpResponse.error();
 });
