49f64f1d1cf803d169e5c17f6151d73eb1184315
Thomas <77514919+TB516@users.noreply.github.com>
2026-03-29 22:29:27 -0400

    Moved generic html runner to use utils

--- src/main/projects/simple-http/graders/generic-html-runner.ts
+++ src/main/projects/simple-http/graders/generic-html-runner.ts
@@ -1,24 +1,17 @@
 import type { GraderResult } from '../../../../shared/types';
+import { contentTypeCheck } from '../../utils/content-type-check';
+import { fetchErrorResult } from '../../utils/fetch-error-result';
+import { httpStatusCheck } from '../../utils/http-status-check';
 
 export const genericHtmlRunner = async (url: string): Promise<GraderResult> => {
   try {
     const response = await fetch(url);
 
-    if (response.status !== 200) {
-      return {
-        status: 'error',
-        message: 'HTTP response status was not 200',
-        details: JSON.stringify(response)
-      };
-    }
+    let failure = httpStatusCheck(response, 200);
+    if (failure) return failure;
 
-    if (response.headers.get('Content-Type') !== 'text/html') {
-      return {
-        status: 'fail',
-        message: 'Content-Type header was not "text/html"',
-        details: JSON.stringify(response)
-      };
-    }
+    failure = contentTypeCheck(response, 'text/html');
+    if (failure) return failure;
 
     return {
       status: 'pass',
@@ -26,10 +19,6 @@ export const genericHtmlRunner = async (url: string): Promise<GraderResult> => {
       details: JSON.stringify(response)
     };
   } catch (e) {
-    return {
-      status: 'error',
-      message: 'A network error occurred',
-      details: JSON.stringify(e)
-    };
+    return fetchErrorResult(e);
   }
 };
