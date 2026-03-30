fe062219a9595809fc80c72d78f47bf841e1ddd7
Thomas <77514919+TB516@users.noreply.github.com>
2026-03-30 01:03:25 -0400

    Updated genericTextRunner to be more flexible

--- src/main/projects/simple-http/graders/generic-text-runner.ts
+++ src/main/projects/simple-http/graders/generic-text-runner.ts
@@ -4,7 +4,11 @@ import { contentTypeCheck } from '../../utils/content-type-check';
 import { fetchErrorResult } from '../../utils/fetch-error-result';
 import { httpStatusCheck } from '../../utils/http-status-check';
 
-export const genericTextRunner = async (url: string, compare: string): Promise<GraderResult> => {
+export const genericTextRunner = async (
+  url: string,
+  expected: string,
+  isEqual: (expected: string, actual: string) => boolean
+): Promise<GraderResult> => {
   const response = await t(fetch, url);
   if (!response.ok) return fetchErrorResult(response.error);
 
@@ -17,8 +21,12 @@ export const genericTextRunner = async (url: string, compare: string): Promise<G
   const text = await t(response.value.text);
   if (!text.ok) return { status: 'error', message: 'Text parsing failed', details: JSON.stringify(text.error) };
 
-  if (text.value !== compare)
-    return { status: 'fail', message: `Expected text to be '${compare}' but '${text.value}' was found instead`, details: JSON.stringify(text.error) };
+  if (!isEqual(expected, text.value))
+    return {
+      status: 'fail',
+      message: `Expected '${expected}', but found '${text.value}'`,
+      details: JSON.stringify(text.error)
+    };
 
   return {
     status: 'pass',
