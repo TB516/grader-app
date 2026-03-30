009ea509d231e684a6710e5e40016dab7c780ec0
Thomas <77514919+TB516@users.noreply.github.com>
2026-03-30 02:53:35 -0400

    Removed the generic runners to have more control in each grader and have each grader have their own test flow

--- src/main/projects/simple-http/graders/hello-text.ts
+++ src/main/projects/simple-http/graders/hello-text.ts
@@ -1,7 +1,32 @@
+import { t } from 'try';
 import type { Grader } from '../../../types';
-import { genericTextRunner } from './generic-text-runner';
+import { contentTypeCheck } from '../../utils/content-type-check';
+import { fetchErrorResult } from '../../utils/fetch-error-result';
+import { httpStatusCheck } from '../../utils/http-status-check';
+
+const expectedText = 'Hello World';
 
 export const helloTextGrader: Grader = {
   label: 'Hello Endpoint Returns "Hello World"',
-  run: (url) => genericTextRunner(url, 'Hello World')
+  run: async (url) => {
+    const response = await t(fetch, url);
+    if (!response.ok) return fetchErrorResult(response.error);
+
+    let failure = httpStatusCheck(response.value, 200);
+    if (failure) return failure;
+
+    failure = contentTypeCheck(response.value, 'text/plain');
+    if (failure) return failure;
+
+    const text = await t(response.value.text);
+    if (!text.ok) return { status: 'error', message: 'Text parsing failed', details: JSON.stringify(text.error) };
+
+    if (text.value === expectedText) return { status: 'fail', message: `Text was not '${expectedText}'`, details: JSON.stringify(response) };
+
+    return {
+      status: 'pass',
+      message: `Text was '${expectedText}'`,
+      details: JSON.stringify(response)
+    };
+  }
 };
--- src/main/projects/simple-http/graders/page-2.ts
+++ src/main/projects/simple-http/graders/page-2.ts
@@ -1,7 +1,24 @@
+import { t } from 'try';
 import type { Grader } from '../../../types';
-import { genericHtmlRunner } from './generic-html-runner';
+import { contentTypeCheck } from '../../utils/content-type-check';
+import { fetchErrorResult } from '../../utils/fetch-error-result';
+import { httpStatusCheck } from '../../utils/http-status-check';
 
 export const page2Grader: Grader = {
   label: 'Page 2 Returns HTML',
-  run: genericHtmlRunner
+  run: async (url) => {
+    const response = await t(fetch, url);
+    if (!response.ok) return fetchErrorResult(response.error);
+
+    let failure = httpStatusCheck(response.value, 200);
+    if (failure) return failure;
+
+    failure = contentTypeCheck(response.value, 'text/html');
+    if (failure) return failure;
+
+    const text = await t(response.value.text);
+    if (!text.ok) return { status: 'error', message: 'Text parsing failed', details: JSON.stringify(text.error) };
+
+    return { status: 'pass', message: 'Page 2 Returned HTML', details: JSON.stringify(response.value) };
+  }
 };
