f5703cf02e4a827442aab29807629224f7e7f50a
Thomas <77514919+TB516@users.noreply.github.com>
2026-03-29 23:13:28 -0400

    Updated spacing

--- src/main/projects/simple-http/graders/generic-html-runner.ts
+++ src/main/projects/simple-http/graders/generic-html-runner.ts
@@ -6,7 +6,6 @@ import { httpStatusCheck } from '../../utils/http-status-check';
 
 export const genericHtmlRunner = async (url: string): Promise<GraderResult> => {
   const [ok, error, response] = await t(fetch, url);
-
   if (!ok) return fetchErrorResult(error);
 
   let failure = httpStatusCheck(response, 200);
