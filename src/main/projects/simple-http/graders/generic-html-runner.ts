8eb8ca3215d9ec834f2724b9c484cebc61245134
Thomas <77514919+TB516@users.noreply.github.com>
2026-03-29 23:07:17 -0400

    Updated generic-html-handler to use try proposal

--- src/main/projects/simple-http/graders/generic-html-runner.ts
+++ src/main/projects/simple-http/graders/generic-html-runner.ts
@@ -1,24 +1,23 @@
+import { t } from 'try';
 import type { GraderResult } from '../../../../shared/types';
 import { contentTypeCheck } from '../../utils/content-type-check';
 import { fetchErrorResult } from '../../utils/fetch-error-result';
 import { httpStatusCheck } from '../../utils/http-status-check';
 
 export const genericHtmlRunner = async (url: string): Promise<GraderResult> => {
-  try {
-    const response = await fetch(url);
+  const [ok, error, response] = await t(fetch, url);
 
-    let failure = httpStatusCheck(response, 200);
-    if (failure) return failure;
+  if (!ok) return fetchErrorResult(error);
 
-    failure = contentTypeCheck(response, 'text/html');
-    if (failure) return failure;
+  let failure = httpStatusCheck(response, 200);
+  if (failure) return failure;
 
-    return {
-      status: 'pass',
-      message: 'Page returned HTML',
-      details: JSON.stringify(response)
-    };
-  } catch (e) {
-    return fetchErrorResult(e);
-  }
+  failure = contentTypeCheck(response, 'text/html');
+  if (failure) return failure;
+
+  return {
+    status: 'pass',
+    message: 'Page returned HTML',
+    details: JSON.stringify(response)
+  };
 };
