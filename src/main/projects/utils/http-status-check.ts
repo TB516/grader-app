c958b7dd0d39baf65db536e4df9c072ba40ec82b
Thomas <77514919+TB516@users.noreply.github.com>
2026-04-01 02:22:08 -0400

    Updated http-status-check to fail instead of error

--- src/main/projects/utils/http-status-check.ts
+++ src/main/projects/utils/http-status-check.ts
@@ -3,7 +3,7 @@ import type { GraderResult } from '../../../shared/types';
 export const httpStatusCheck = (response: Response, code: number): GraderResult | null => {
   if (response.status !== code) {
     return {
-      status: 'error',
+      status: 'fail',
       message: `HTTP response status was not "${code}"`,
       details: JSON.stringify(response)
     };
